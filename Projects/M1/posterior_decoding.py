22cf6d35019c6604662da84f9c2196a0b7f45269
ashlin-k <ashlin.kanawaty@gmail.com>
2020-03-21 11:42:33 -0400

    Continuing to write posterior_decoding.py

--- Projects/M1/posterior_decoding.py
+++ Projects/M1/posterior_decoding.py
@@ -12,7 +12,7 @@
 
 import sys
 from math import log
-from util import plothist
+import numpy as np
 
 ###############################################################################
 # HMM PARAMETERS
@@ -43,54 +43,102 @@ em = [
 ]
 
 
-def fwd_bkw(sequence):
-    """Forward–backward algorithm."""
+def posteriorDecode(sequence):
 
     fmatrix = [[0 for col in range(len(sequence))] for row in range(len(state_idx))]
-    bmatrix = [[0 for col in range(len(sequence))] for row in range(len(state_idx))]
+    bmatrix = [[0 for col in range(len(sequence))] for row in range(len(state_idx))]    
 
     # Forward part of the algorithm
-    for i in range(len(sequence)):    	
-    	iBase = base_idx[sequence[i]]        
+    for i in range(0, len(sequence)):    	
+        iBase = base_idx[sequence[i]]        
         for st in state_idx:
-        	iState = state_idx[st]
+            iState = state_idx[st]
             if i == 0:
                 fsumPrev = init_dist[iState]
             else:
-                fsumPrev = sum(fmatrix[k][i-1]*tr[k][iState] for k in state_idx)
+                fsumPrev = sum(fmatrix[state_idx[k]][i-1]*tr[state_idx[k]][iState] for k in state_idx)
 
             fmatrix[iState][i] = em[iState][iBase] * fsumPrev
 
-
-    p_fwd = sum(fmatrix[k][len(sequence)-1] * final_dist[state_idx[k]] for k in state_idx) # ??? we don't know where it will end
+    # calculate the probability of all emissions
+    p_fwd = sum(fmatrix[state_idx[k]][len(sequence)-1] * final_dist[state_idx[k]] for k in state_idx)
 
     # Backward part of the algorithm
-
-
-
-
-
-
-    bkw = []
-    b_prev = {}
-    for i, observation_i_plus in enumerate(reversed(observations[1:]+(None,))):
-        b_curr = {}
+    for i in range(len(sequence)-1, -1, -1):
+        iBase = base_idx[sequence[i]]
         for st in state_idx:
-            if i == 0:
-                # base case for backward part
-                b_curr[st] = tr[st][end_st]
+            iState = state_idx[st]
+            if i == len(sequence)-1:
+                bmatrix[iState][i] = final_dist[iState]
             else:
-                b_curr[st] = sum(tr[st][l] * em[l][observation_i_plus] * b_prev[l] for l in state_idx)
+                iBase = base_idx[sequence[i+1]] 
+                bmatrix[iState][i] = sum(em[state_idx[l]][iBase]*tr[state_idx[l]][iState]*bmatrix[state_idx[l]][iBase] for l in state_idx)
 
-        bkw.insert(0,b_curr)
-        b_prev = b_curr
-
-    p_bkw = sum(init_dist[l] * em[l][observations[0]] * b_curr[l] for l in state_idx)
+    # iBase = base_idx[sequence[0]]
+    # p_bkw = sum(init_dist[l] * em[l][iBase] * bmatrix[state_idx[l]][iBase] for l in state_idx)
 
     # Merging the two parts
-    posterior = []
-    for i in range(len(observations)):
-        posterior.append({st: fwd[i][st] * bkw[i][st] / p_fwd for st in state_idx})
+    posterior = [[0 for col in range(len(sequence))] for row in range(len(state_idx))]
+    if p_fwd == 0:
+        print "P_fwd is zero!"
+        return False, fmatrix, bmatrix, posterior
+    for i in range(0, len(sequence)):
+        for st in state_idx:
+            iState = state_idx[st]
+            iBase = base_idx[sequence[i]]
+            posterior[iState][i] = fmatrix[iState][i] * bmatrix[iState][i] / p_fwd
 
-    assert p_fwd == p_bkw
-    return fwd, bkw, posterior
+    return True, fmatrix, bmatrix, posterior
+
+def main():
+
+    # parse command-line arguments
+    # if len(sys.argv) < 2:
+    #     print "you must call program as: ./posterior_decoding.py <datafile>"
+    #     sys.exit(1)
+    # datafile = sys.argv[1]
+    datafile = "PS1/hmmgen"
+
+    # read sequences
+    print "reading sequence"
+    f = open(datafile)
+    X = f.readline()
+    # refanno = f.readline()
+    f.close()
+
+    if X[len(X)-1] == '\n': X=X[0:len(X)-1]
+    # if refanno[len(refanno)-1] == '\n': refanno=refanno[0:len(refanno)-1]
+
+    X=list(X)
+    # refanno=list(refanno)
+    # for i in xrange(len(X)):
+    #     X[i] = base_idx[X[i]]
+    # for i in xrange(len(refanno)):
+    #     refanno[i] = state_idx[refanno[i]]
+
+    # perform posterior decoding
+    print "performing posterior decoding"
+    success, fmatrix, bmatrix, posterior = posteriorDecode(X)
+
+    if not success:
+        print "PD failed, exiting now"
+        exit(1)
+
+    # find the path containing the most likely state at each position
+    print "finding path"
+    N = len(posterior[0])
+    pi = []
+    for i in range(0, N):
+        # find the state with the max probability and add to pi
+        maxState = '+'   
+        for st in state_idx:
+            if st != maxState and posterior[iState][i] > posterior[state_idx[maxState]][i]:
+                maxState = st
+        pi.append(maxState)
+    
+    print "-" * 15
+    print "The path containing the most likely state at each position is"
+    print pi
+
+if __name__ == "__main__":
+    main()
